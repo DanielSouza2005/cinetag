7a28cab9fd214fed3fc3e30215681941073bef39
DanielSouza2005 <danisimoes741@gmail.com>
2025-01-11 15:19:45 -0300

    Implemented the Cabecalho, CabecalhoLink and Rodape components

---
+++ src/components/Rodape/index.js
@@ -0,0 +1,11 @@
+import styles from "./Rodape.module.css";
+
+const Rodape = () => {
+    return (
+        <footer className={styles.rodape}>
+            <p>Desenvolvido por Daniel Simões</p>
+        </footer>
+    );
+};
+
+export default Rodape;
---
+++ src/pages/Inicio/index.js
@@ -0,0 +1,15 @@
+import styles from "./Inicio.module.css";
+import Cabecalho from "components/Cabecalho";
+import Rodape from "components/Rodape";
+
+const Inicio = () => {
+    return (
+        <>
+            <Cabecalho />
+
+            <Rodape />
+        </>
+    );
+};
+
+export default Inicio;
